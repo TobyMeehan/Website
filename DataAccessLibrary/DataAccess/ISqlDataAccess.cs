d258e94e413d69efcb9ad2b021a790e9f89d532c
Toby <toby@tobymeehan.com>
2020-02-02 20:02:40 +0000

    Add SqlDataAccess
    Added data access class and interface for dependency injection.

---
+++ DataAccessLibrary/DataAccess/ISqlDataAccess.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary.DataAccess
+{
+    public interface ISqlDataAccess
+    {
+        string ConnectionStringName { get; set; }
+
+        Task<List<T>> LoadData<T>(string sql, object parameters = null);
+        Task SaveData(string sql, object parameters);
+    }
+}
---
+++ DataAccessLibrary/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary.DataAccess
+{
+    public class SqlDataAccess : ISqlDataAccess
+    {
+        private readonly IConfiguration _config;
+
+        public string ConnectionStringName { get; set; } = "Default";
+
+        public SqlDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task<List<T>> LoadData<T>(string sql, object parameters = null)
+        {
+            string connectionString = _config.GetConnectionString(ConnectionStringName);
+
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                var data = await connection.QueryAsync<T>(sql, parameters);
+
+                return data.ToList();
+            }
+        }
+
+        public async Task SaveData(string sql, object parameters)
+        {
+            string connectionString = _config.GetConnectionString(ConnectionStringName);
+
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                await connection.ExecuteAsync(sql, parameters);
+            }
+        }
+    }
+}
