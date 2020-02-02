dede597bc7c11a11fc8a3d84b4f41fe25db4544a
Toby <toby@tobymeehan.com>
2020-02-02 19:56:49 +0000

    Add connection strings to API configuration.
    Added connection string config file and added to IConfiguration.

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -18,6 +18,10 @@ namespace WebApi
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddJsonFile("connectionStrings.config");
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
