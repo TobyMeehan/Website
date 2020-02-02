36b91baf78698150548145bb7d4f5c81d747255b
Toby <toby@tobymeehan.com>
2020-02-02 21:06:08 +0000

    Add API models.
    Added some user interface models for the API.

---
+++ WebApi/Models/RoleModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class RoleModel
+    {
+        public int RoleId { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ WebApi/Models/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class UserModel
+    {
+        public string UserId { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public List<RoleModel> Roles { get; set; }
+
+    }
+}
