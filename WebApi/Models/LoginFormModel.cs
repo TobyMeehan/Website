8ec8298c08e7a7b15d46a89d360af2cc10100dde
Toby <toby@tobymeehan.com>
2020-02-04 21:30:44 +0000

    Replace LoginViewModel.
    Replaced the LoginViewModel with a new LoginFormModel as it was causing some issues in the editor.

---
+++ WebApi/Models/LoginFormModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class LoginFormModel
+    {
+        [Required(ErrorMessage = "Enter your username.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Enter your password.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
