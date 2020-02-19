76d2408f1000d5c3227515243c32d20bc0cc637c
Toby <toby@tobymeehan.com>
2020-02-19 20:46:17 +0000

    Add AccountController.
    Added account controller for clients to access their user information.

---
+++ WebApi/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using DataAccessLibrary;
+using DataAccessLibrary.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class AccountController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IUserProcessor _userProcessor;
+        private readonly IApplicationProcessor _applicationProcessor;
+        private readonly IConnectionProcessor _connectionProcessor;
+
+        public AccountController(IMapper mapper, IUserProcessor userProcessor, IApplicationProcessor applicationProcessor, IConnectionProcessor connectionProcessor)
+        {
+            _mapper = mapper;
+            _userProcessor = userProcessor;
+            _applicationProcessor = applicationProcessor;
+            _connectionProcessor = connectionProcessor;
+        }
+
+        public async Task<UserModel> Get()
+        {
+            return _mapper.Map<UserModel>(await _userProcessor.GetUserById(User.Identity.Name));
+        }
+    }
+}
