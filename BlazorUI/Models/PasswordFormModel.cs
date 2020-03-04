51c2d5e901122203367c3691c147179806f34dda
Toby Meehan <toby@tobymeehan.com>
2020-03-04 18:13:55 +0000

    Add account page
    Add page for users to view and change account details

---
+++ BlazorUI/Models/PasswordFormModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorUI.Models
+{
+    public class PasswordFormModel
+    {
+        [Required(ErrorMessage = "Enter your password.")]
+        public string Password { get; set; }
+
+    }
+}
---
+++ BlazorUI/wwwroot/scripts/Settings.Index.js
@@ -0,0 +1,11 @@
+﻿function Settings_Index_ShowUsernameTab() {
+    $('#InformationTabs a[href="#UsernameContent"]').tab('show');
+}
+
+function Settings_Index_ShowPasswordTab() {
+    $('#InformationTabs a[href="#PasswordContent"]').tab('show');
+}
+
+function Settings_Index_ShowAccountTab() {
+    $('#InformationTabs a[href="#AccountContent"]').tab('show');
+}
