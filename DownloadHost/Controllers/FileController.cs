0c544e6715da01b423299b4693cb8c02f4837db8
Toby <toby@tobymeehan.com>
2020-03-06 23:15:13 +0000

    Add upload and download actions for download
    Added API actions for uploading, downloading and storing files.

---
+++ DownloadHost/Controllers/FileController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DownloadHost.Controllers
+{
+    public class FileController : Controller
+    {
+        [Route("/{download}/{file}")]
+        public IActionResult Index(string download, string file)
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "Files", download, file);
+
+            return PhysicalFile(path, System.Net.Mime.MediaTypeNames.Application.Octet);
+        }
+    }
+}
---
+++ DownloadHost/Controllers/UploadController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DownloadHost.Controllers
+{    
+    public class UploadController : Controller
+    {
+        [HttpPost]
+        [Route("/upload/{download}/{filename}")]
+        public async Task<IActionResult> Index([FromQuery]string download,[FromQuery]string filename)
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "Files", download, filename);
+
+            using var writer = System.IO.File.OpenWrite(path);
+            await Request.Body.CopyToAsync(writer);
+
+            return Ok();
+        }
+    }
+}
