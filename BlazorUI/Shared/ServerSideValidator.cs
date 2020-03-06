c6e4b5c8cc38f606dbb476eeadcacce67ab54959
Toby Meehan <toby@tobymeehan.com>
2020-03-06 16:25:07 +0000

    Initial change account actions
    Started ability to change account information from account page

---
+++ BlazorUI/Shared/ServerSideValidator.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorUI.Shared
+{
+    public class ServerSideValidator : ComponentBase
+    {
+        private ValidationMessageStore _messageStore;
+
+        [CascadingParameter] EditContext CurrentEditContext { get; set; }
+
+        /// <inheritdoc />
+        protected override void OnInitialized()
+        {
+            if (CurrentEditContext == null)
+            {
+                throw new InvalidOperationException($"{nameof(ServerSideValidator)} requires a cascading " +
+                    $"parameter of type {nameof(EditContext)}. For example, you can use {nameof(ServerSideValidator)} " +
+                    $"inside an {nameof(EditForm)}.");
+            }
+
+            _messageStore = new ValidationMessageStore(CurrentEditContext);
+            CurrentEditContext.OnValidationRequested += (s, e) => _messageStore.Clear();
+            CurrentEditContext.OnFieldChanged += (s, e) => _messageStore.Clear(e.FieldIdentifier);
+        }
+
+        public void DisplayErrors(Dictionary<string, List<string>> errors)
+        {
+            foreach (var err in errors)
+            {
+                _messageStore.Add(CurrentEditContext.Field(err.Key), err.Value);                
+            }
+            CurrentEditContext.NotifyValidationStateChanged();
+        }
+    }
+
+}
