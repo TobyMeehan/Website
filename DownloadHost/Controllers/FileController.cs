cc9be0864f059ef7d58d65a632b59ad8f325e800
Toby <toby@tobymeehan.com>
2020-03-17 20:00:25 +0000

    Further improvement of DownloadHost
    Various error fixes and added /download redirect to the download host

--- DownloadHost/Controllers/FileController.cs
+++ DownloadHost/Controllers/FileController.cs
@@ -10,11 +10,25 @@ namespace DownloadHost.Controllers
     public class FileController : Controller
     {
         [Route("/{download}/{file}")]
-        public IActionResult Index(string download, string file)
+        public IActionResult Get(string download, string file)
         {
             string path = Path.Combine(Directory.GetCurrentDirectory(), "Files", download, file);
 
             return PhysicalFile(path, System.Net.Mime.MediaTypeNames.Application.Octet);
+        }
+
+        [Route("/{download}/{file}")]
+        [HttpDelete]
+        public IActionResult Delete(string download, string file)
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "Files", download, file);
+
+            if (System.IO.File.Exists(path))
+            {
+                System.IO.File.Delete(path);
+            }
+
+            return Ok();
         }
     }
 }
--- DownloadHost/Controllers/UploadController.cs
+++ DownloadHost/Controllers/UploadController.cs
@@ -7,19 +7,22 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DownloadHost.Controllers
-{    
+{
     public class UploadController : Controller
     {
         [HttpPost]
         [Route("/upload/{download}/{filename}")]
-        public async Task<IActionResult> Index([FromQuery]string download,[FromQuery]string filename)
+        public async Task<IActionResult> Index(string download, string filename, [FromBody]string content)
         {
-            string path = Path.Combine(Directory.GetCurrentDirectory(), "Files", download, filename);
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "Files", download);
+            Directory.CreateDirectory(path);
+            path = Path.Combine(path, filename);
+
+            byte[] data = Convert.FromBase64String(content);
 
             using var writer = System.IO.File.OpenWrite(path);
-            await Request.Body.CopyToAsync(writer);
-
-            return Ok();
+            await writer.WriteAsync(data);
+            return Created($"/{download}/{filename}", filename);
         }
     }
 }
