6c29da7935f9805413b957eaee160b7a3a9dbe40
Toby <toby@tobymeehan.com>
2020-03-19 16:12:16 +0000

    Add page titles
    Added component to set the page title

---
+++ BlazorUI/Shared/Title.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorUI.Shared
+{
+    public class Title : ComponentBase
+    {
+        [Inject] protected IJSRuntime jsRuntime { get; set; }
+
+        [Parameter] public string Value { get; set; }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                Value = (!string.IsNullOrWhiteSpace(Value)) ? $"{Value} - " : "";
+                await SetTitle($"{Value}TobyMeehan");
+            }
+        }
+
+        private async Task SetTitle(string title)
+        {
+            await jsRuntime.InvokeVoidAsync("setTitle", title);
+        }
+    }
+}
