1a2476d39dfa0ef23da63ec62d3f091c010944c9
Toby <toby@tobymeehan.com>
2020-04-03 18:51:56 +0100

    Change compare attributes
    Modified compare attributes to suit whether the model is used by blazor or razor.

--- BlazorUI/Models/ChangePasswordFormModel.cs
+++ BlazorUI/Models/ChangePasswordFormModel.cs
@@ -16,12 +16,12 @@ namespace BlazorUI.Models
         [Required(ErrorMessage = "Please enter new password.")]
         [Display(Name = "New Password")]
         [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be between 8 - 100 characters.")]
-        [CompareProperty(nameof(ConfirmNewPassword), ErrorMessage = "Passwords must match.")] // TODO: experimental library :/
         [DataType(DataType.Password)]
         public string NewPassword { get; set; }
 
         [Required(ErrorMessage = "Please confirm new password.")]
         [Display(Name = "Confirm New Password")]
+        [CompareProperty(nameof(NewPassword), ErrorMessage = "Passwords must match.")]
         [DataType(DataType.Password)]
         public string ConfirmNewPassword { get; set; }
 
