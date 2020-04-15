bb332aacc5cbc40287a032bcd35b8d57623aa550
Toby <toby@tobymeehan.com>
2020-04-15 19:05:15 +0100

    Add custom input formatter for form data
    Added formatter so controllers can handle form data in addition to json

---
+++ WebApi/FormDataInputFormatter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Formatters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi
+{
+    public class FormDataInputFormatter : IInputFormatter
+    {
+        public bool CanRead(InputFormatterContext context)
+        {
+            return context.HttpContext.Request.HasFormContentType;
+        }
+
+        public Task<InputFormatterResult> ReadAsync(InputFormatterContext context)
+        {
+            return InputFormatterResult.SuccessAsync(null);
+        }
+    }
+}
