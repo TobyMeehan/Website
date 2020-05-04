894b8a4162a446fe6bdcb194ff6ab7b9213fb14f
Toby <toby@tobymeehan.com>
2020-05-04 18:20:48 +0100

    Add file manager for download host
    Added managers for file handling

---
+++ DownloadHost/Data/IFileManager.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace DownloadHost.Data
+{
+    public interface IFileManager
+    {
+        string GenerateRandomFilename(string downloadId);
+        Task SaveFile(string downloadId, string filename, Stream stream);
+        void ZipUpFile(string downloadId, string filename);
+    }
+}
