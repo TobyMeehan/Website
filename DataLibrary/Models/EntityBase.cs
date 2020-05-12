1d5d6907d20f2620c901c5969f9db0fb5c9a872a
Toby <toby@tobymeehan.com>
2020-05-12 22:01:50 +0100

    Add auto automapper
    Added automapper functionality within EntityBase

--- DataLibrary/Models/EntityBase.cs
+++ DataLibrary/Models/EntityBase.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace TobyMeehan.Com.Data.Models
 {
     public abstract class EntityBase
     {
         public string Id { get; set; }
+
+        public T As<T>(IMapper mapper)
+        {
+            return mapper.Map<T>(this);
+        }
+
+        public Task<T> AsAsync<T>(IMapper mapper)
+        {
+            return Task.Run(() => mapper.Map<T>(this));
+        }
     }
 }
