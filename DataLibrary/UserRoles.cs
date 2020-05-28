c6add1dccde65fdc201f20c66a6a9cc0b99a2c88
Toby <toby@tobymeehan.com>
2020-05-28 15:28:21 +0100

    Add user components
    Added razor components for displaying users

---
+++ DataLibrary/UserRoles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TobyMeehan.Com.Data
+{
+    public class UserRoles
+    {
+        public const string Admin = "Admin";
+
+        public const string Verified = "Verified";
+    }
+}
---
+++ TobyMeehan.Com/Components/AttributableComponentBase.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TobyMeehan.Com.Components
+{
+    public class AttributableComponentBase : ComponentBase
+    {
+        private Dictionary<string, object> _additionalAttributes;
+
+        [Parameter(CaptureUnmatchedValues = true)]
+        public Dictionary<string, object> AdditionalAttributes
+        {
+            get => _additionalAttributes;
+            
+            set
+            {
+                if (value.TryGetValue("class", out object cssClass))
+                {
+                    CssClass = cssClass.ToString();
+                }
+
+                _additionalAttributes = value.Where(x => x.Key != "class").ToDictionary(x => x.Key, x => x.Value);
+            }
+        }
+
+        protected string CssClass { get; set; }
+    }
+}
