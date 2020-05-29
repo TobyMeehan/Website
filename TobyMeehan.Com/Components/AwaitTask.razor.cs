d88306c237b84c1e93403083ee8f7aaed67899db
Toby <toby@tobymeehan.com>
2020-05-29 15:52:49 +0100

    Update await task component
    Changed to wait for task rather than model

---
+++ TobyMeehan.Com/Components/AwaitTask.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TobyMeehan.Com.Components
+{
+    public partial class AwaitTask<T> : ComponentBase
+    {
+        [Parameter] public Task<T> Task { get; set; }
+        [Parameter] public RenderFragment<T> ChildContent { get; set; }
+
+        private T _item;
+
+        protected override async Task OnInitializedAsync()
+        {
+            _item = await Task;
+        }
+    }
+}
