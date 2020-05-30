240eb8e55772e0807730c50b51b271947ac42c29
Toby <toby@tobymeehan.com>
2020-05-30 18:48:15 +0100

    Add transactions page
    Added account settings page for transactions

---
+++ TobyMeehan.Com/Pages/Settings/Transactions.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TobyMeehan.Com.Data;
+using TobyMeehan.Com.Data.Models;
+using TobyMeehan.Com.Extensions;
+
+namespace TobyMeehan.Com.Pages.Settings
+{
+    public partial class Transactions : ComponentBase
+    {
+        [CascadingParameter] public Task<AuthenticationState> AuthenticationStateTask { get; set; }
+        [CascadingParameter] public User CurrentUser { get; set; }
+    }
+}
