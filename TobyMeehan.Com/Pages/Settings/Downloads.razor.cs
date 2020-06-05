6725b1fa8d5629795d43a62c2cf7fea0df79112a
Toby <toby@tobymeehan.com>
2020-06-05 23:56:51 +0100

    Add download settings page
    Added download page for account settings area

---
+++ TobyMeehan.Com/Pages/Settings/Downloads.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TobyMeehan.Com.Data;
+using TobyMeehan.Com.Data.Extensions;
+using TobyMeehan.Com.Data.Models;
+
+namespace TobyMeehan.Com.Pages.Settings
+{
+    public partial class Downloads : ComponentBase
+    {
+        [Inject] private IRepository<Download> downloads { get; set; }
+
+        [CascadingParameter] public User CurrentUser { get; set; }
+
+        private Task<IEnumerable<Download>> DownloadTask()
+        {
+            return downloads.GetByUser(CurrentUser.Id);
+        }
+    }
+}
