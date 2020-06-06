44154bf91a48f172aea80f7af31c38b05d52bb8f
Toby <toby@tobymeehan.com>
2020-06-06 16:33:16 +0100

    Add download index page

---
+++ TobyMeehan.Com/Pages/Downloads/Index.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TobyMeehan.Com.Data;
+using TobyMeehan.Com.Data.Models;
+
+namespace TobyMeehan.Com.Pages.Downloads
+{
+    public partial class Index : ComponentBase
+    {
+        [Inject] private IRepository<Download> downloads { get; set; }
+
+        private IList<Download> _downloads;
+
+        protected override async Task OnInitializedAsync()
+        {
+            _downloads = await downloads.GetAsync();
+        }
+    }
+}
