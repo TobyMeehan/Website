9529da829663c177f7a8a543c1564cdc7b91e758
Toby <toby@tobymeehan.com>
2020-06-07 15:42:50 +0100

    Add challenge component
    Added challenge and redirect components for authorization

--- TobyMeehan.Com/Extensions/NavigationManagerExtensions.cs
+++ TobyMeehan.Com/Extensions/NavigationManagerExtensions.cs
@@ -13,9 +13,14 @@ namespace TobyMeehan.Com.Extensions
             return $"/{navigation.ToBaseRelativePath(navigation.Uri)}";
         }
 
+        public static string LoginUrl(this NavigationManager navigation, string endpoint = "login")
+        {
+            return $"/{endpoint}?ReturnUrl={System.Net.WebUtility.UrlEncode(navigation.ToBaseRelativePath())}";
+        }
+
         private static void NavigateToAuth(this NavigationManager navigation, string endpoint)
         {
-            navigation.NavigateTo($"/{endpoint}?ReturnUrl={System.Net.WebUtility.UrlEncode(navigation.ToBaseRelativePath())}", true);
+            navigation.NavigateTo(navigation.LoginUrl(endpoint), true);
         }
 
         public static void NavigateToLogin(this NavigationManager navigation) => navigation.NavigateToAuth("login");
