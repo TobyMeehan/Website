1506b4948fc25b6787eca8233ece1b80da7b5aa2
Toby <toby@tobymeehan.com>
2020-06-07 14:57:08 +0100

    Add connection repository
    Added specific repository for oauth connections

---
+++ DataLibrary/Repositories/IConnectionRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TobyMeehan.Com.Data.Models;
+
+namespace TobyMeehan.Com.Data.Repositories
+{
+    public interface IConnectionRepository
+    {
+        Task<IList<Connection>> GetAsync();
+
+        Task<Connection> GetByIdAsync(string id);
+
+        Task<IList<Connection>> GetByUserAsync(string userId);
+
+        Task<IList<Connection>> GetByApplicationAsync(string appId);
+
+        Task<Connection> AddAsync(string userId, string appId);
+
+        Task DeleteAsync(string id);
+    }
+}
