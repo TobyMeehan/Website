247164cdd872546aa249c6e1e4544d9d4b45ee06
Toby <toby@tobymeehan.com>
2020-06-08 16:45:15 +0100

    Add download files page
    Added blazor page for download file manager, with upload action

---
+++ TobyMeehan.Com/Pages/Downloads/Files.razor.cs
@@ -0,0 +1,77 @@
+﻿using Blazor.FileReader;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TobyMeehan.Com.Data.Models;
+using TobyMeehan.Com.Data.Repositories;
+using TobyMeehan.Com.Data.Upload;
+using TobyMeehan.Com.Extensions;
+using TobyMeehan.Com.Tasks;
+
+namespace TobyMeehan.Com.Pages.Downloads
+{
+    public partial class Files : ComponentBase
+    {
+        [Inject] private IDownloadRepository downloadRepository { get; set; }
+        [Inject] private ProgressTaskState taskState { get; set; }
+        [Inject] private EditDownloadState editDownloadState { get; set; }
+
+        [Parameter] public string Id { get; set; }
+
+        private Download _download;
+        private List<string> _files = new List<string>();
+        private IEnumerable<FileUploadTask> _uploadTasks = new List<FileUploadTask>();
+        private readonly int _maxSize = 100 * 1024 * 1024;
+
+        protected override async Task OnInitializedAsync()
+        {
+            _download = await downloadRepository.GetByIdAsync(Id);
+            _files = _download.Files.Select(f => f.Filename).ToList();
+
+            editDownloadState.Id = _download.Id;
+            editDownloadState.Title = _download.Title;
+
+            taskState.OnStateChanged += () =>
+            {
+                _uploadTasks = taskState.Tasks
+                    .Where(t => t is FileUploadTask file && file.Download == _download && !file.IsComplete())
+                    .Select(f => f as FileUploadTask);
+                StateHasChanged();
+            };
+        }
+
+        private async Task FileUpload_Change(IEnumerable<IFileReference> files)
+        {
+            foreach (var file in files)
+            {
+                var fileInfo = await file.ReadFileInfoAsync();
+
+                if (fileInfo.Size > _maxSize)
+                {
+                    return;
+                }
+
+                Func<CancellationToken, IProgress<IUploadProgress>, Task> uploadTask = async (cancellationToken, progress) =>
+                {
+                    using Stream uploadStream = await file.OpenReadAsync();
+                    await downloadRepository.AddFileAsync(_download.Id, fileInfo.Name, uploadStream, cancellationToken, progress);
+                };
+
+                var progressTask = new FileUploadTask(fileInfo.Name, _download, uploadTask);
+
+                progressTask.OnComplete += t => _files.Add(fileInfo.Name);
+
+                taskState.Add(progressTask);
+            }
+        }
+
+        private async Task DeleteFile_Click(DownloadFile file)
+        {
+
+        }
+    }
+}
--- TobyMeehan.Com/Tasks/FileUploadTask.cs
+++ TobyMeehan.Com/Tasks/FileUploadTask.cs
@@ -11,13 +11,23 @@ namespace TobyMeehan.Com.Tasks
 {
     public class FileUploadTask : ProgressTaskBase
     {
-        public FileUploadTask(Func<CancellationToken, Progress<IUploadProgress>, Task> task, string filename, Download download)
+        public FileUploadTask(string filename, Download download, Func<CancellationToken, Progress<IUploadProgress>, Task> task)
         {
             Progress<IUploadProgress> progress = new Progress<IUploadProgress>();
 
             progress.ProgressChanged += (s, e) =>
             {
                 PercentageProgress = e.PercentageProgress;
+
+                Status = e.Status switch
+                {
+                    UploadStatus.NotStarted => TaskStatus.InProgress,
+                    UploadStatus.Starting => TaskStatus.InProgress,
+                    UploadStatus.Uploading => TaskStatus.InProgress,
+                    UploadStatus.Completed => TaskStatus.Completed,
+                    UploadStatus.Failed => TaskStatus.Failed
+                };
+
                 NotifyProgressChanged();
             };
 
