48824d2a5df89ac2aaba6cdcb13a177a4d5f0d7a
Toby <toby@tobymeehan.com>
2020-06-08 16:44:22 +0100

    Add progress bar component
    Added component for progress bar

---
+++ TobyMeehan.Com/BootstrapContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TobyMeehan.Com
+{
+    public enum BootstrapContext
+    {
+        Primary,
+        Secondary,
+        Success,
+        Danger,
+        Warning,
+        Info,
+        Light,
+        Dark
+    }
+}
