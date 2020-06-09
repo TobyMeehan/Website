34b282c1af6ac40e2718c23e03c935cc627bacc4
Toby <toby@tobymeehan.com>
2020-06-09 22:14:42 +0100

    Update edit download authorization handler
    Added exemption for admin role

--- TobyMeehan.Com/Authorization/CanEditDownloadAuthorizationHandler.cs
+++ TobyMeehan.Com/Authorization/CanEditDownloadAuthorizationHandler.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TobyMeehan.Com.Data;
 using TobyMeehan.Com.Data.Models;
 using TobyMeehan.Com.Extensions;
 
@@ -12,7 +13,7 @@ namespace TobyMeehan.Com.Authorization
     {
         protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, EditDownloadRequirement requirement, Download resource)
         {
-            if (resource.Authors.Any(u => u.Id == context.User.Id()))
+            if (resource.Authors.Any(u => u.Id == context.User.Id()) || context.User.IsInRole(UserRoles.Admin))
             {
                 context.Succeed(requirement);
             }
