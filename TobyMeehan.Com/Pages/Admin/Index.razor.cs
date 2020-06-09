a3e6502039a787d5cafa211512fe6dec7242d628
Toby <toby@tobymeehan.com>
2020-06-09 21:03:40 +0100

    Add admin page
    Added admin blazor page to list all users

---
+++ TobyMeehan.Com/Pages/Admin/Index.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TobyMeehan.Com.Data.Models;
+using TobyMeehan.Com.Data.Repositories;
+
+namespace TobyMeehan.Com.Pages.Admin
+{
+    public partial class Index : ComponentBase
+    {
+        [Inject] public IUserRepository users { get; set; }
+
+        private IList<User> _users;
+
+        protected override async Task OnInitializedAsync()
+        {
+            _users = await Task.Run(users.GetAsync);
+        }
+    }
+}
