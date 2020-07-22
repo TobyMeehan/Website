03d669947eede2627d4f30d0bf53f6cbb9714c30
Toby <toby@tobymeehan.com>
2020-07-22 16:15:43 +0100

    Add button data access
    Added data access and repository for the button

---
+++ DataLibrary/Repositories/IButtonRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TobyMeehan.Com.Data.Models;
+
+namespace TobyMeehan.Com.Data.Repositories
+{
+    public interface IButtonRepository
+    {
+        Task<IList<ButtonPress>> GetAsync();
+
+        Task<IList<ButtonPress>> GetByUserAsync(string userId);
+
+        Task AddAsync(string userId, TimeSpan buttonTimeSpan);
+    }
+}
---
+++ DataLibrary/Repositories/SqlButtonRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TobyMeehan.Com.Data.Models;
+using TobyMeehan.Sql;
+
+namespace TobyMeehan.Com.Data.Repositories
+{
+    public class SqlButtonRepository : SqlRepository<ButtonPress>, IButtonRepository
+    {
+        private readonly ISqlTable<ButtonPress> _table;
+
+        public SqlButtonRepository(ISqlTable<ButtonPress> table) : base(table)
+        {
+            _table = table;
+        }
+
+        public Task AddAsync(string userId, TimeSpan buttonTimeSpan)
+        {
+            string id = Guid.NewGuid().ToString();
+
+            return _table.InsertAsync(new
+            {
+                Id = id,
+                UserId = userId,
+                TimePressed = DateTime.Now,
+                ButtonSeconds = buttonTimeSpan.Seconds
+            });
+        }
+
+        public async Task<IList<ButtonPress>> GetByUserAsync(string userId)
+        {
+            return (await _table.SelectByAsync(b => b.UserId == userId)).ToList();
+        }
+    }
+}
