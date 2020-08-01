0a8f27716046340da24c43c0303c86495b5513d9
Toby <toby@tobymeehan.com>
2020-08-01 15:55:56 +0100

    Add initial api configuration
    Added configuration for services and authentication to api startup

--- TobyMeehan.Com.Api/Startup.cs
+++ TobyMeehan.Com.Api/Startup.cs
@@ -2,12 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.VisualBasic.CompilerServices;
+using MySql.Data.MySqlClient;
+using TobyMeehan.Com.Api.Models;
+using TobyMeehan.Com.Data.Configuration;
+using TobyMeehan.Com.Data.Models;
+using TobyMeehan.Com.Data.Security;
 
 namespace TobyMeehan.Com.Api
 {
@@ -23,7 +32,56 @@ namespace TobyMeehan.Com.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDataAccessLibrary()
+                .AddSqlDatabase(() => new MySqlConnection(Configuration.GetConnectionString("Default")))
+                .AddBCryptPasswordHash();
+
+            var tokenProvider = new RsaTokenProvider("api.tobymeehan.com", "OAuth Application", Guid.NewGuid().ToString().ToUpperInvariant());
+            services.AddSingleton<ITokenProvider>(tokenProvider);
+
+            services.AddSingleton(ConfigureMapper());
+
             services.AddControllersWithViews();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = tokenProvider.GetValidationParameters();
+            });
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+            {
+#if DEBUG
+                options.Cookie.Domain = "localhost";
+#else
+                options.Cookie.Domain = ".tobymeehan.com";
+#endif
+
+                options.Events = new CookieAuthenticationEvents
+                {
+                    OnRedirectToLogin = context =>
+                    {
+                        context.HttpContext.Response.Redirect("https://tobymeehan.com/login");
+                        return Task.CompletedTask;
+                    },
+                    OnRedirectToAccessDenied = context =>
+                    {
+                        context.HttpContext.Response.Redirect("https://tobymeehan.com/login");
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+        }
+
+        private IMapper ConfigureMapper()
+        {
+            return new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Connection, ConnectionModel>().ReverseMap();
+                cfg.CreateMap<Application, ApplicationModel>().ReverseMap();
+                cfg.CreateMap<User, UserModel>().ReverseMap();
+                cfg.CreateMap<Role, RoleModel>().ReverseMap();
+            }).CreateMapper();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +99,8 @@ namespace TobyMeehan.Com.Api
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseAuthentication();
 
             app.UseRouting();
 
