020b2cb154e17e6ed97c7cecfb9e662469b743a7
Toby <toby@tobymeehan.com>
2020-08-03 17:33:36 +0100

    Update user controller
    Changed identify scope logic and added list all users action

--- TobyMeehan.Com.Api/Controllers/Api/UserController.cs
+++ TobyMeehan.Com.Api/Controllers/Api/UserController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using TobyMeehan.Com.Api.Authorization;
+using TobyMeehan.Com.Api.Models;
 using TobyMeehan.Com.Api.Models.Api;
 using TobyMeehan.Com.Data.Repositories;
 
@@ -29,6 +30,15 @@ namespace TobyMeehan.Com.Api.Controllers.Api
 
         [HttpGet]
         [Authorize]
+        public async Task<IActionResult> Get()
+        {
+            var users = await _users.GetAsync();
+
+            return Ok(_mapper.Map<List<PartialUserResponse>>(users));
+        }
+
+        [HttpGet]
+        [Authorize]
         public async Task<IActionResult> Get(string id)
         {
             if (id == "@me")
@@ -36,14 +46,14 @@ namespace TobyMeehan.Com.Api.Controllers.Api
                 id = UserId;
             }
 
-            var user = await _users.GetByIdAsync(id);
+            var user = _mapper.Map<UserModel>(await _users.GetByIdAsync(id));
 
             if (user == null)
             {
                 return NotFound(new ErrorResponse("User does not exist."));
             }
 
-            if (HasScope(Scopes.Identify))
+            if ((await _authorizationService.AuthorizeAsync(User, user, new IdentifyScopeRequirement())).Succeeded)
             {
                 return Ok(_mapper.Map<UserResponse>(user));
             }
