ac8cedb1269f877d80112a12482a7828527bd1e0
Toby <toby@tobymeehan.com>
2020-08-03 23:54:26 +0100

    Add application controller
    Added api controller for applications

---
+++ TobyMeehan.Com.Api/Models/Api/ApplicationResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TobyMeehan.Com.Api.Models.Api
+{
+    public class ApplicationResponse
+    {
+        public string Name { get; set; }
+        public string UserId { get; set; }
+        public string Description { get; set; }
+        public string IconUrl { get; set; }
+    }
+}
