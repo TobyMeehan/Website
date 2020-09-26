b88e20716ac8e5c59a984514c758a0aad7411035
Toby <toby@tobymeehan.com>
2020-09-26 17:42:50 +0100

    Update api ui
    Removed default views and added favicons

--- TobyMeehan.Com.Api/Controllers/HomeController.cs
+++ TobyMeehan.Com.Api/Controllers/HomeController.cs
@@ -18,13 +18,7 @@ namespace TobyMeehan.Com.Api.Controllers
 
         public IActionResult Index()
         {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return Redirect("https://tobymeehan.com/developer");
         }
     }
 }
