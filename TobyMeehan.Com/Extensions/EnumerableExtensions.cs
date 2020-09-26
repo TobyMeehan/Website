0ded9c6667598bcbc3b733157fa6c0263bdf4faa
Toby <toby@tobymeehan.com>
2020-09-26 17:21:07 +0100

    Update transactions page
    Added pagination and correct time formatting

---
+++ TobyMeehan.Com/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TobyMeehan.Com.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static int IndexOf<T>(this IEnumerable<T> source, T value)
+        {
+            int index = 0;
+
+            foreach (T item in source)
+            {
+                if (item.Equals(value))
+                {
+                    return index;
+                }
+
+                index++;
+            }
+
+            return -1;
+        }
+    }
+}
--- TobyMeehan.Com/Pages/Settings/Transactions.razor.cs
+++ TobyMeehan.Com/Pages/Settings/Transactions.razor.cs
@@ -6,13 +6,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using TobyMeehan.Com.Data;
 using TobyMeehan.Com.Data.Models;
+using TobyMeehan.Com.Data.Repositories;
 using TobyMeehan.Com.Extensions;
 
 namespace TobyMeehan.Com.Pages.Settings
 {
     public partial class Transactions : ComponentBase
     {
+        [Inject] private ITransactionRepository transactions { get; set; }
+
         [CascadingParameter] public Task<AuthenticationState> AuthenticationStateTask { get; set; }
         [CascadingParameter] public User CurrentUser { get; set; }
+
+        private List<IGrouping<DateTime, Transaction>> _transactions;
+
+        protected override async Task OnInitializedAsync()
+        {
+            var ts = await Task.Run(async () => await transactions.GetByUserAsync(CurrentUser.Id));
+
+            _transactions = ts.OrderBy(x => x.Sent).GroupBy(x => x.Sent.Date).Reverse().ToList();
+        }
     }
 }
