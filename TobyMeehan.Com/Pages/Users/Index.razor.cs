45969c5a366022cee1ba5f45ce2cf009af5469e7
Toby <toby@tobymeehan.com>
2021-03-21 00:16:15 +0000

    Update users page
    Updated user profile pages to use vanity url, and added an index page listing all users.

---
+++ TobyMeehan.Com/Pages/Users/Index.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TobyMeehan.Com.Data.Models;
+using TobyMeehan.Com.Data.Repositories;
+
+namespace TobyMeehan.Com.Pages.Users
+{
+    public partial class Index : ComponentBase
+    {
+        [Inject] private IUserRepository users { get; set; }
+
+        private IList<User> _users;
+
+        protected override async Task OnInitializedAsync()
+        {
+            _users = await Task.Run(() => users.GetAsync());
+        }
+    }
+}
