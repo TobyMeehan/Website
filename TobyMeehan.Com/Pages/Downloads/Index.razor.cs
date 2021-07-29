814c5fb827e59bd15a4787964bb600cceb48e1b8
Toby <toby@tobymeehan.com>
2021-07-29 18:06:18 +0100

    Add unlisted list
    Added unlisted downloads owned by user to top of downloads page

--- TobyMeehan.Com/Pages/Downloads/Index.razor.cs
+++ TobyMeehan.Com/Pages/Downloads/Index.razor.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TobyMeehan.Com.AspNetCore.Extensions;
 using TobyMeehan.Com.Data;
 using TobyMeehan.Com.Data.Models;
 using TobyMeehan.Com.Data.Repositories;
@@ -13,11 +15,21 @@ namespace TobyMeehan.Com.Pages.Downloads
     {
         [Inject] private IDownloadRepository downloads { get; set; }
 
+        [CascadingParameter] public Task<AuthenticationState> AuthenticationStateTask { get; set; }
+
         private IList<Download> _downloads;
+        private IList<Download> _unlisted;
+
+        private AuthenticationState _context;
 
         protected override async Task OnInitializedAsync()
         {
+            _context = await AuthenticationStateTask;
+
             var list = await Task.Run(downloads.GetAsync);
+            var unlist = await Task.Run(() => downloads.GetByAuthorAsync(_context.User.Id()));
+
+            _unlisted = unlist.Where(d => d.Visibility == DownloadVisibility.Unlisted).ToList();
             _downloads = list.Where(d => d.Visibility == DownloadVisibility.Public).ToList();
         }
     }
