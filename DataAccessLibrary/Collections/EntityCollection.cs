0408a4f692a9cf47a9780b9b4066816a21e9fdac
Toby <toby@tobymeehan.com>
2021-07-29 16:27:49 +0100

    Update entity collections
    Moved to collections namespace and added interface

---
+++ DataAccessLibrary/Collections/EntityCollection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TobyMeehan.Com.Data.Models;
+
+namespace TobyMeehan.Com.Data.Collections
+{
+    public class EntityCollection<T> : IEntityCollection<T> where T : EntityBase
+    {
+        public EntityCollection() { }
+
+        public EntityCollection(IEnumerable<T> collection)
+        {
+            _items = collection.ToList();
+        }
+
+        private List<T> _items = new List<T>();
+
+        public T this[string id] => _items.First(i => i.Id == id);
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _items.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ DataAccessLibrary/Collections/IEntityCollection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TobyMeehan.Com.Data.Models;
+
+namespace TobyMeehan.Com.Data.Collections
+{
+    public interface IEntityCollection<T> : IEnumerable<T> where T : EntityBase
+    {
+        T this[string id] { get; }
+    }
+}
