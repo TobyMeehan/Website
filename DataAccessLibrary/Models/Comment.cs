ee0fcd875228f6864528c31954e3aedb1a162f76
Toby <toby@tobymeehan.com>
2021-07-30 18:27:15 +0100

    Update comment repository
    Added SqlKata implementation

--- DataAccessLibrary/Models/Comment.cs
+++ DataAccessLibrary/Models/Comment.cs
@@ -6,8 +6,13 @@ using TobyMeehan.Sql.QueryBuilder;
 namespace TobyMeehan.Com.Data.Models
 {
     [SqlName("comments")]
-    public class Comment : MessageBase
+    public class Comment : EntityBase
     {
+        public string UserId { get; set; }
+        public User User { get; set; }
+        public string Content { get; set; }
+        public DateTime Sent { get; set; }
+        public DateTime? Edited { get; set; }
         public string EntityId { get; set; }
     }
 }
