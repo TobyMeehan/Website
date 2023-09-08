9d3b4b692b01fc327f16b1b49d3d29e0f2d166cb
TobyMeehan <toby@tobymeehan.com>
2023-09-08 13:31:15 +0100

    Add tailwind styles
    Added tailwind styling to login and register pages

--- src/TobyMeehan.Com.Accounts/tailwind.config.js
+++ src/TobyMeehan.Com.Accounts/tailwind.config.js
@@ -1,8 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: [],
+  content: [
+      "Pages/**/*.cshtml"
+  ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      
+      'deep': '#030712',
+      'dark': '#111827',
+      'base': '#1f2937',
+      'content': '#2b3440',
+      'flash': '#374151',
+      
+      'light': {
+        DEFAULT: '#d1d5db',
+        hover: '#6b7280',
+        focus: '#4b5563'
+      },
+      
+      'primary': {
+        DEFAULT: '#2b3440',
+        hover: '#262e38',
+        focus: '#262e38'
+      },
+      
+      'link': {
+        DEFAULT: '#0284c7',
+        hover: '#0369a1'
+      },
+      
+      'positive': '#16a34a',
+      'negative': '#dc2626',
+      'neutral': '#ca8a04'
+    },
     extend: {},
   },
   plugins: [],
