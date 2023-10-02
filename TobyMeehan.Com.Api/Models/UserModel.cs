d4e36744aedcd752f47fe30cdbb3998921b57da6
TobyMeehan <toby@tobymeehan.com>
2023-10-02 13:51:09 +0100

    Remove api entity collection
    Removed custom collection causing mapping error

--- TobyMeehan.Com.Api/Models/UserModel.cs
+++ TobyMeehan.Com.Api/Models/UserModel.cs
@@ -10,6 +10,6 @@ namespace TobyMeehan.Com.Api.Models
         public string Username { get; set; }
         public int Balance { get; set; }
         public string ProfilePictureUrl { get; set; }
-        public EntityCollection<RoleModel> Roles { get; set; }
+        public IList<RoleModel> Roles { get; set; }
     }
 }
